4d2fd8973f33539e538d1f883b26db88a0f8a773
shanfeng <1218154980@qq.com>
2021-09-08 21:06:47 +0800

    队列的学习

--- 队列/index.js
+++ 队列/index.js
@@ -4,3 +4,15 @@ queue.push(2);
 const item1=queue.shift();
 const item2=queue.shift();
 console.log(item1,item2,queue);
+
+var RecentCounter=function(){
+  this.q=[]
+}
+
+RecentCounter.prototype.ping=function(t){
+  this.q.push(t);
+  while(this.q[0]<t-3000){
+    this.q.shift();
+  }
+  return this.q.length;
+}
