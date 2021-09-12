563a0df65e3534cfac8fa9c0f7f2b51e517fb243
shanfeng <1218154980@qq.com>
2021-09-12 08:21:52 +0800

    防抖节流深入理解

--- 防抖和节流/debounce.js
+++ 防抖和节流/debounce.js
@@ -1,3 +1,17 @@
+// 实例
+// 用户频繁操作，只执行最后一次，如果触发事件不超过0.5s，
+let t=null
+input.oninput=function(){
+  // 如果不是最后一次
+  if(t){
+    clearTimeout(t)
+  }
+  t=setTimeout(()=>{
+    // 业务逻辑
+    console.log(this.value);
+  },500)
+}
+
 
 // 防抖
 function debounce(fn, delay) {
@@ -10,6 +24,24 @@ function debounce(fn, delay) {
       fn.call(this)
     }, delay);
   }
+}
+
+// 实例
+// 控制执行次数,每隔多少秒执行一次
+window.onscroll=function(){
+  console.log(123);
+}
+
+let flag=true;
+window.onscroll=function(){
+  // 如果falg为true，只执行一次
+  if(flag){
+    setTimeout(()=>{
+      console.log('hello');
+      flag=true;
+    },500)
+  }
+  flag=false;
 }
 // 节流
 function throttle(fn, delay) {
